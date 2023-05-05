174aa19eb362ee37a97fe206f21b8ca96a833eb5
zzw-math <zhongziwen@whu.edu.cn>
2023-05-05 13:49:24 +0100

    little modification

--- stack_folder/application_with_stack.py
+++ stack_folder/application_with_stack.py
@@ -23,17 +23,17 @@ print(l)
 def is_matched(expr):
     """ Return True if all delimiters are properly match; False otherwise.
     """
-    left = '({[' # opening delims
-    right = ')}]' # closing delims
+    left = '({['  # opening delims
+    right = ')}]'  # closing delims
     S = ArrayStack()
     for c in expr:
         if c in left:
-            S.push(c) # push left delim on stack
+            S.push(c)  # push left delim on stack
         elif c in right:
             if S.is_empty():
-                return False # nothing to match with
-            if right.index(c) != left.index(S.pop()): # 做了两件事
-                return False # mismatched
+                return False  # nothing to match with
+            if right.index(c) != left.index(S.pop()):  # 做了两件事
+                return False  # mismatched
     return S.is_empty()
 
 
@@ -51,17 +51,17 @@ def is_matched_html(raw):
     S = ArrayStack()           # find first '<'
     j = raw.find('<')
     while j != -1:
-        k = raw.find('>', j+1) # find next '>'
+        k = raw.find('>', j+1)  # find next '>'
         if k == -1:
-            return False # invalid tag
-        tag = raw[j+1:k] # strip away < >
-        if not tag.startswith('/'): # this is opening tag
+            return False  # invalid tag
+        tag = raw[j+1:k]  # strip away < >
+        if not tag.startswith('/'):  # this is opening tag
             S.push(tag)
-        else: # this is closing tag
+        else:  # this is closing tag
             if S.is_empty():
-                return False # nothing to match with
+                return False  # nothing to match with
             if tag[1:] != S.pop():
-                return False # mismatched # find next '<'
+                return False  # mismatched # find next '<'
         j = raw.find('<', k+1)
     return S.is_empty()
 
